43ae396d07eec1ac2bfec578becc67d5daec3c80
allendai1 <65299178+allendai1@users.noreply.github.com>
2021-07-19 23:13:37 -0400

    '.'

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var cors = require('cors')
 app.use(express.static("static"));
 app.use(cors())
 
-console.log("wok");
+console.log("wokky");
 
 app.get("/audio", (req, res) => {
 	const yt_link  = req.query.yt_link;
